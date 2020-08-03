e371fd670fc351a63f2859fba762a10cb7edb864
Sankalp_Trivedi <46197276+jarvis149@users.noreply.github.com>
2020-08-03 20:38:00 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -208,3 +208,15 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+const stars = document.querySelectorAll("#star");
+
+stars.forEach((star) => {
+  let duration = Math.random() * (1.2 - 0.6) + 0.6;
+  star.style.animation = `stars ${duration}s infinite linear`;
+});
+
+window.addEventListener("load", () => {
+  const preloader = document.querySelector(".preloader");
+  preloader.classList.add("preload-finish");
+});
+
